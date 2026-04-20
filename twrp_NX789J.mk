4e5b53f5e47fc44fdb2e67a29f77f790e1fa4e7a
Kancy Joe <54024877+sunflower2333@users.noreply.github.com>
2026-04-20 18:02:53 +0800

    Update device identifiers to lowercase format

--- twrp_NX789J.mk
+++ twrp_NX789J.mk
@@ -4,20 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/nubia/NX789J
+DEVICE_PATH := device/nubia/nx789J
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Device identifier
-PRODUCT_DEVICE := NX789J
-PRODUCT_NAME := twrp_NX789J
+PRODUCT_DEVICE := nx789J
+PRODUCT_NAME := twrp_nx789J
 PRODUCT_BRAND := nubia
 PRODUCT_MANUFACTURER := nubia
 PRODUCT_MODEL := RedMagic 10 Pro
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := NX789J
+TARGET_OTA_ASSERT_DEVICE := nx789J
 
 # Fingerprint
 BUILD_FINGERPRINT := nubia/NX789J-UN/NX789J:15/AQ3A.240812.002/RedMagicOS10.0.15_NX789J_GB:user/release-keys
