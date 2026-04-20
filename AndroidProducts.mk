ed4ebf4a9eedc3258e44cc346df603ba6691396f
Kancy Joe <54024877+sunflower2333@users.noreply.github.com>
2026-04-20 18:03:44 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,5 +6,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_nx789j.mk
 COMMON_LUNCH_CHOICES := \
-    twrp_NX789J-ap3a-eng \
-    twrp_NX789J-ap3a-userdebug
+    twrp_nx789J-ap3a-eng \
+    twrp_nx789J-ap3a-userdebug
